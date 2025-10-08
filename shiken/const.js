4f1cf2f677609a12af6303fc836b86a5c26fa8da
Vu Ngoc Trung <vungoctrung93@gmail.com>
2025-10-08 10:09:29 +0700

    adding động từ

--- shiken/const.js
+++ shiken/const.js
@@ -1,2 +1,2 @@
-export const setInputbyJP = ['KANJI1', 'KANJI2', 'BUNPO1', 'trường âm'];
+export const setInputbyJP = ['KANJI1', 'KANJI2', 'BUNPO1', 'trường âm', 'ĐT nhóm 2 đặc biệt', 'Động từ'];
 export const setInputHalfPoint = ['HIRAGANA'];
