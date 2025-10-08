0342804e0fd048281bd5d51dc65dcd2cfa5731ea
vungoctrung93 <vungoctrung93@gmail.com>
2025-10-08 22:56:23 +0700

    mixing data set

--- shiken/const.js
+++ shiken/const.js
@@ -1,2 +1,2 @@
-export const setInputbyJP = ['KANJI1', 'KANJI2', 'BUNPO1', 'trường âm', 'ĐT nhóm 2 đặc biệt', 'Động từ'];
+export const setInputbyJP = ['KANJI1', 'KANJI2', 'BUNPO1', 'trường âm', 'ĐT bất quy tắc', 'Động từ'];
 export const setInputHalfPoint = ['HIRAGANA'];
